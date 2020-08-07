3bd04e9b6713d47441e4063dc17614b42eb99df5
Jorge Hernandez <jehernandezrodriguez@gmail.com>
2020-08-07 12:55:53 -0500

    Change version code

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -42,6 +42,7 @@ public class AppConstants {
 
 
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@jehernandezrodriguez/ClouDi", "assets://shell-app-manifest.json", "application/json"));
